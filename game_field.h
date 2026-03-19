f2665e5c12b5206a39847e5d2148f97f943ba8ee
Andriic14 <gumenukand2004@gmail.com>
2026-03-19 22:10:18 +0200

    Update game_field.h

--- game_field.h
+++ game_field.h
@@ -2,20 +2,35 @@
 #define MIN_SIZE 3
 #define MAX_SIZE 8
 
-struct GameField
-{
-	int** board;
-	int size;
-	// Координати порожньої клітинки
-	int emptyRow;
-	int emptyCol;
+enum GameState {
+	Active,
+	Win,
+	Interrupted
 };
 
-// Ініціалізація поля(виділяє пам'ять, заповнює числами)
-void initField(GameField& field, int size);
-// Виводить поточний стан поля у консоль
-void printField(const GameField& field);
-// Звільняє динамічну пам'ять
-void freeField(GameField& field);
-// Перемішує фішки для початку нової гри
-void shuffleField(GameField& field);
+struct GameField
+{
+    int** board;
+    int size;
+
+    // Координати порожньої клітинки
+    int emptyRow;
+    int emptyCol;
+
+    GameState state; // стан гри
+
+    // Ініціалізація поля (виділяє пам'ять, заповнює числами)
+    void init(int d);
+    // Виводить поточний стан поля у консоль
+    void print() const;
+    // Звільняє динамічну пам'ять
+    void freeField();
+    // Перемішує фішки для початку нової гри
+    void shuffleField();
+    // Перевірка можливості ходу
+    bool canMove(int row, int col) const;
+    // Виконання ходу
+    void makeMove(int tile);
+    // Перевірка умови виграшу
+    bool checkWin() const;
+};
