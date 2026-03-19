632ab554dede22fde3ff7a348a9867bb7597ad11
Andriic14 <gumenukand2004@gmail.com>
2026-03-19 22:09:38 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,6 +13,13 @@ int main() {
 	cout << "    Гра П'ятнашки        " << endl;
 	cout << "==========================" << endl;
 	cout << "Розмір поля від " << MIN_SIZE << " до " << MAX_SIZE << endl;
+
+	int size;
+	cout << "Введіть розмір поля: ";
+	cin >> size;
+	cout << "\n";
 	GameController ctrl;
+	ctrl.field.init(size);
+	ctrl.field.print();
 	return 0;
 }
