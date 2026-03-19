946edc2a0c32d1bf2697cff289f6cb1f4a8f7f5a
Andriic14 <gumenukand2004@gmail.com>
2026-03-19 22:11:25 +0200

    Update game_ctrl.h

--- game_ctrl.h
+++ game_ctrl.h
@@ -3,18 +3,15 @@
 
 struct GameController
 {
-	GameField field;
-	int moveCount;
-	bool isRunning;
-};
+    GameField field;
+    int moveCount;
 
-// Запускає нову гру: ініціалізує поле, обнуляє лічильник ходів,
-void startGame(GameController& ctrl);
-// Обробляє введення користувача з клавіатури.
-void processInput(GameController& ctrl);
-// Виконує хід у вказаному напрямку.
-bool makeMove(GameController& ctrl, char direction);
-// Перевіряє чи гравець виграв
-bool checkWin(const GameController& ctrl);
-// Завершує гру
-void endGame(GameController& ctrl);
+    // Запускає нову гру: ініціалізує поле, обнуляє лічильник ходів
+    void startGame();
+
+    // Обробляє введення користувача – повертає введене ціле значення
+    int processInput();
+
+    // Основний ігровий цикл
+    void gameLoop();
+};
