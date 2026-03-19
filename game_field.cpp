f547bde2e40cf9a421a22c6656fa0e232d032661
Andriic14 <gumenukand2004@gmail.com>
2026-03-19 22:10:57 +0200

    Update game_field.cpp

--- game_field.cpp
+++ game_field.cpp
@@ -1,19 +1,55 @@
 #include "game_field.h"
 #include <iostream>
+#include <iomanip>
+
 using namespace std;
 
-void initField(GameField& field, int  size) {
+void GameField::init(int d) {
+	size = d;
+	board = new int* [d];
+	for (int i = 0; i < d; i++) {
+		board[i] = new int[d];
+	}
+	int num = size * size - 1;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			board[i][j] = num;
+			num--;
+		}
+	}
 
+	board[size - 1][size - 1] = 0;
+	emptyRow = size - 1;
+	emptyCol = size - 1;
+	if (size % 2 == 0) {
+		int temp = board[size - 1][size - 2];
+		board[size - 1][size - 2] = board[size - 1][size - 3];
+		board[size - 1][size - 3] = temp;
+	}
+
+	state = Active;
 }
 
-void printField(const GameField& field) {
-
+void GameField::print() const {
+	for (int i = 0; i < size; i++){
+		for (int j = 0; j < size; j++) {
+			if (board[i][j] == 0) {
+				cout << setw(3) << "_";
+			}
+			else {
+				cout << setw(3) << board[i][j];
+			}
+		}
+		cout << endl;
+	}
 }
 
-void freeField(GameField& field) {
+void GameField::freeField() {}
 
-}
+void GameField::shuffleField() {}
 
-void shuffleField(GameField& field) {
+bool GameField::canMove(int row, int col) const { return false; }
 
-}
+void GameField::makeMove(int tile) {}
+
+bool GameField::checkWin() const { return false; }
