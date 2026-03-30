9041339e81a02d8c2c131c03b872f2057d1e0f6d
Andriic14 <gumenukand2004@gmail.com>
2026-03-30 19:21:59 +0300

    Update game_field.cpp

--- game_field.cpp
+++ game_field.cpp
@@ -44,12 +44,55 @@ void GameField::print() const {
 	}
 }
 
-void GameField::freeField() {}
+void GameField::freeField() {
+	for (int i = 0; i < size; i++) {
+		delete[] board[i];
+	}
+	delete[] board;
+	board = nullptr;
+}
 
 void GameField::shuffleField() {}
 
-bool GameField::canMove(int row, int col) const { return false; }
+bool GameField::canMove(int row, int col) const { 
+	if (row - 1 == emptyRow && col == emptyCol) return true;
+	if (row + 1 == emptyRow && col == emptyCol) return true;
+	if (row == emptyRow && col - 1 == emptyCol) return true;
+	if (row == emptyRow && col + 1 == emptyCol) return true;
+	return false; 
+}
 
-void GameField::makeMove(int tile) {}
+bool GameField::makeMove(int tile) {
+	int tileRow = -1, tileCol = -1;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (board[i][j] == tile) {
+				tileRow = i;
+				tileCol = j;
+			}
+		}
+	}
+	if (tileRow == -1) return false;
+	if (!canMove(tileRow, tileCol)) return false;
+	board[emptyRow][emptyCol] = tile;
+	board[tileRow][tileCol] = 0;
 
-bool GameField::checkWin() const { return false; }
+	emptyRow = tileRow;
+	emptyCol = tileCol;
+
+	return true;
+}
+
+bool GameField::checkWin() const { 
+	int expected = 1;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (i == size - 1 && j == size - 1) {
+				return board[i][j] == 0;
+			}
+			if (board[i][j] != expected) return false;
+			expected++;
+		}
+	}
+	return false; 
+}
