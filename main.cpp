23d2717495858dd5d4ad4925d19dec053dfe8997
Andriic14 <gumenukand2004@gmail.com>
2026-03-30 19:21:36 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -20,6 +20,66 @@ int main() {
 	cout << "\n";
 	GameController ctrl;
 	ctrl.field.init(size);
+
+	cout << "Початковий стан поля:" << endl;
 	ctrl.field.print();
+	// фішка зліва від порожньої
+	int r = size - 1, c = size - 2;
+	cout << "\nЧи можна перемістити фішку [" << r << "][" << c << "] = " << ctrl.field.board[r][c] << "?" << endl;
+	if (ctrl.field.canMove(r, c)) {
+		cout << "Так, хід можливий" << endl;
+	}
+	else {
+		cout << "Ні, хід неможливий" << endl;
+	}
+	// фішка в кутку
+	cout << "\nЧи можна перемістити фішку [0][0] = "
+		<< ctrl.field.board[0][0] << "?" << endl;
+	if (ctrl.field.canMove(0, 0))
+		cout << "Так, хід можливий" << endl;
+	else
+		cout << "Ні, хід неможливий" << endl;
+
+	// виконання ходу
+	int tile = ctrl.field.board[size - 1][size - 2];
+	cout << "\nВиконуємо хід — переміщуємо фішку " << tile << ":" << endl;
+	ctrl.field.makeMove(tile);
+	ctrl.field.print();
+
+
+	// ще хід
+	int tile2 = ctrl.field.board[size - 2][size - 2];
+	cout << "\nВиконуємо хід — переміщуємо фішку " << tile2 << ":" << endl;
+	ctrl.field.makeMove(tile2);
+	ctrl.field.print();
+
+	// перевірка виграшу
+	cout << "\nПеревірка виграшу після ходів: ";
+	if (ctrl.field.checkWin()) {
+		cout << "Виграш" << endl;
+	}
+	else {
+		cout << "Гра ще не завершена" << endl;
+	}
+
+	// вручну ставимо виграшний стан
+	int num = 1;
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			ctrl.field.board[i][j] = num++;
+		}
+	}
+	ctrl.field.board[size - 1][size - 1] = 0;
+	ctrl.field.emptyRow = size - 1;
+	ctrl.field.emptyCol = size - 1;
+
+	cout << "\nВиграшний стан поля:" << endl;
+	ctrl.field.print();
+	cout << "Перевірка виграшу: ";
+	if (ctrl.field.checkWin())
+		cout << "Виграш!" << endl;
+	else
+		cout << "Гра ще не завершена" << endl;
+
 	return 0;
 }
