83f8dbc0ef9e5e603d8c2557b58b91c63f7ce667
Andriic14 <gumenukand2004@gmail.com>
2026-04-09 20:48:18 +0300

    Update game_ctrl.cpp

--- game_ctrl.cpp
+++ game_ctrl.cpp
@@ -3,8 +3,56 @@
 
 using namespace std;
 
-void GameController::startGame() {}
+void GameController::startGame() {
+    int size;
+    do {
+        cout << "Введіть розмір поля (" << MIN_SIZE << "-" << MAX_SIZE << "): ";
+        cin >> size;
+        cin.ignore();
+        if (size<MIN_SIZE || size>MAX_SIZE) {
+            cout << "Невірний розмір. Спробуйте ще раз." << endl;
+        }
+    } while (size < MIN_SIZE || size > MAX_SIZE);
 
-int GameController::processInput() { return 0; }
+    field.init(size);
+    moveCount = 0;
+    gameLoop();
+}
 
-void GameController::gameLoop() {}
+int GameController::processInput() {
+    cout << "Введіть номер фішки (пробіл = вихід): ";
+    char ch;
+    cin.get(ch);
+    if (ch == ' ') return 32;
+    cin.putback(ch);
+    int tile;
+    cin >> tile;
+    cin.ignore();
+    return tile;
+}
+
+void GameController::gameLoop() {
+    while (field.state == Active) {
+        cout << "\n";
+        cout << field;
+        int input = processInput();
+        if (input == 32) {
+            field.state = Interrupted;
+            cout << "\nГру перервано." << endl;
+            break;
+        }
+        bool moved = field ^ input;
+        if (!moved) {
+            cout << "Неможливо перемістити фішку " << input << endl;
+        }
+        else {
+            moveCount++;
+        }
+        if (field.checkWin()) {
+            field.state = Win;
+            cout << "\n";
+            cout << field;
+            cout << "Вітаємо! Ви виграли за " << moveCount << " ходів." << endl;
+        }
+    }
+}
